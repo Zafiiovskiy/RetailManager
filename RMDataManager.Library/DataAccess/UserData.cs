a1ea654e45e07899669bb1d49be05f007743639c
Andrian Zafiiovskiy <andrian.zafiiovskiy@gmail.com>
2020-06-03 00:30:42 +0300

    Add DataAccess
    Adding classes for acces to User data. Adding stored procedure.

---
+++ RMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,25 @@
+﻿using RMDataManager.Library.ManagingDataFiles.DataAccess;
+using RMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "RMData");
+            foreach(UserModel user in output){
+                user.Id = Id;
+            }
+            return output;
+        }
+    }
+}
---
+++ RMDataManager/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace RMDataManager.Controllers
+{
+    [System.Web.Http.Authorize]
+    public class UserController : ApiController
+    {
+     
+        public UserModel GetById()
+        {
+            string id = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+            
+            return data.GetUserById(id).First();
+        }
+
+    }
+}
